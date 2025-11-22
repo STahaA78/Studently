c0d0d3436545a1b2bfdf6c4dd2ae700a76773bfd
MoizPasha <133485761+MoizPasha@users.noreply.github.com>
2025-11-22 14:26:55 +0500

    feat: connected flutter app to firebase

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screens/login_page.dart';  // Make sure this path matches your file location
 import 'package:device_preview/device_preview.dart';
+// Firebase imports
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
     runApp(
     DevicePreview(
       enabled: true, // Set to false to disable Device Preview
