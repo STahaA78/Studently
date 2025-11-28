a5f3efad06e784ae98b7851daa3aa3551ec993b1
MoizPasha <133485761+MoizPasha@users.noreply.github.com>
2025-11-28 15:41:08 +0500

    feat: added the Auth Service class for firebase authentication

--- flutter/lib/auth_service.dart
+++ flutter/lib/auth_service.dart
@@ -1,7 +1,75 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
-class Auth {
+ValueNotifier<AuthService> authService = ValueNotifier(AuthService());
+
+class AuthService {
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
   User? get currentUser => firebaseAuth.currentUser;
+
+  Stream<User?> get authStateChanges => firebaseAuth.authStateChanges();
+
+  Future<User?> signIn({
+    required String email,
+    required String password,
+  }) async {
+    final userCredential = await firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password
+    );
+    return userCredential.user;
+  }
+
+  Future<User?> createAccount({
+    required String email,
+    required String password,
+  }) async {
+    final userCredential = await firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password
+    );
+    return userCredential.user;
+  }
+
+  Future<void> signOut() async {
+    await firebaseAuth.signOut();
+  }
+
+  Future<void> updateUsername({
+    required String username
+  }) async {
+    await currentUser?.updateDisplayName(username);
+  }
+  
+  Future<void> resetPassword({
+    required String email
+  }) async {
+    await firebaseAuth.sendPasswordResetEmail(email: email);
+  }
+
+  Future<void> deleteAccount({
+    required String email,
+    required String password,
+  }) async {
+    AuthCredential credential = EmailAuthProvider.credential(
+      email: email,
+      password: password
+    );
+    await currentUser?.reauthenticateWithCredential(credential);
+    await currentUser?.delete();
+    await firebaseAuth.signOut();
+  }
+
+  Future<void> updatePassword({
+    required String currentPassword,
+    required String newPassword,
+  }) async {
+    AuthCredential credential = EmailAuthProvider.credential(
+      email: currentUser?.email ?? '',
+      password: currentPassword
+    );
+    await currentUser?.reauthenticateWithCredential(credential);
+    await currentUser?.updatePassword(newPassword);
+  }
 }
