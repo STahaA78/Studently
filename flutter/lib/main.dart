b104c87cc97c77e445f3135a931bf18b91ff575a
STahaA78 <syedtahaahmed78@gmail.com>
2026-03-06 23:21:56 +0500

    Just Changed the Main.py of Flutter to ensure login page is the startup

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -8,7 +8,7 @@ import 'package:email_otp/email_otp.dart';
 //import 'screens/community_feed_page.dart';
 //import 'screens/knowledge_hub_page.dart';
 import 'screens/add_resource_page.dart';
-//import 'screens/login_page.dart';  // Make sure this path matches your file location
+import 'screens/login_page.dart';  // Make sure this path matches your file location
 import 'package:studently/models/course.dart';
 
 void main() async {
@@ -23,13 +23,13 @@ void main() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-    // runApp(
-    //   DevicePreview(
-    //     enabled: true, // Set to false to disable Device Preview
-    //     builder: (context) => MyApp(), // Wrap your app
-    //   ),
-    // );
-    runApp(const MyApp());
+    runApp(
+      DevicePreview(
+        enabled: true, // Set to false to disable Device Preview
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
+    //runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -46,14 +46,14 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      //home: LoginPage(), // directly show the login page
+      home: LoginPage(), // directly show the login page
       //home: CommunityFeedPage(),
-      home: AddResourcePage(
-        course: Course(
-          name: "Artificial Intelligence",
-          code: "AI2002"
-        )
-      ),
+      // home: AddResourcePage(
+      //   course: Course(
+      //     name: "Artificial Intelligence",
+      //     code: "AI2002"
+      //   )
+      // ),
       
       // Global theme settings
       theme : ThemeData(
--- flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,26 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
+import file_picker
+import file_selector_macos
 import firebase_auth
 import firebase_core
+import flutter_image_compress_macos
+import path_provider_foundation
 import shared_preferences_foundation
+import syncfusion_pdfviewer_macos
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
